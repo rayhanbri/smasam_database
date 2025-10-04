c1b2aa5066a6c15bc43cd3a7b47664168290e5c1
rayhan <mohammadrayhanx0@gmail.com>
2025-10-04 20:23:10 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running with npm i express!");
-});
+const client = new MongoClient(process.env.MONGODB_URI);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run();
+
+
+
